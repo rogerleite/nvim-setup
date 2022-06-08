3ad7f71d2ac128cfc2138c86850964adb26ef075
Roger Leite <roger.barreto@gmail.com>
2022-06-08 18:14:12 -0300

    Add nvim-tree as file explorer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -72,6 +72,10 @@ return packer.startup(function(use)
   -- Treesitter
   use "nvim-treesitter/nvim-treesitter"
 
+  -- Icons && Nvim Tree file explorer
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

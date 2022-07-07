ac4cb1819e98655873a712406c792973b4608966
Roger Leite <roger.barreto@gmail.com>
2022-07-07 17:51:09 -0300

    Change Conjure configuration default

---
+++ lua/user/conjure.lua
@@ -0,0 +1,3 @@
+-- https://github.com/Olical/conjure/blob/master/doc/conjure-client-clojure-nrepl.txt
+vim.g["conjure#client#clojure#nrepl#eval#auto_require"] = false
+vim.g['conjure#client#clojure#nrepl#connection#auto_repl#enabled'] = false
